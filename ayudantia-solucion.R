46f318af1c40d557caa252d935f9464ed18b343b
sofia <samadariaga@uc.cl>
2023-10-02 17:00:58 -0300

    update ultimas correcciones

--- ayudantia-solucion.R
+++ ayudantia-solucion.R
@@ -1,6 +1,6 @@
 
-# Borrar todos los objetos, ¿Cómo se realizaba esta acción?
-
+# Borrar todos los objetos
+rm(lsit = ls())
 
 # Si no tiene instalado algunos de los paquetes que estána  continuación, instale las instrucciones:
 install.packages("tidyverse")
@@ -44,19 +44,41 @@ head(elsoc)
 
 # 1) Elabore un histograma de la variable edad.
 
+elsoc %>% 
+  ggplot(aes(x = edad)) +
+  geom_histogram()
 
-# 2) Elabore un gráfico de barras con el porcentaje de encuestados ‘tendencia“.
-# Luego, agrupe los datos según los años.
+# 2) Elabore un gráfico de barras con el porcentaje de encuestados ‘tendencia“. ¿Es posible anteponer
+# un gráfico de líneas? Luego, agrupe los datos según los años.
 
-# Gráfico 1
+elsoc %>% 
+  mutate(annio = factor(annio)) %>%
+  ggplot(aes(x = tendencia)) +
+  geom_bar()
 
-# Gráfico 2
+elsoc %>% 
+  mutate(annio = factor(annio)) %>%
+  group_by(annio) %>% 
+  count(tendencia) %>% 
+  mutate(prop = n/sum(n)) %>% 
+  ggplot(aes(x = tendencia, y = prop, fill = annio)) +
+  geom_bar(stat = "identity", position = position_dodge2())
 
 # BONUS: Añada porcentaje
 
 # 3) De gran interés es observar cómo ha variado la confianza en el gobierno a lo largo de los años.
 # Elabore un gráfico de líneas del promedio de confianza_gobierno por anio. Luego, genere la
 # mejor visualización para agrupar estos promedios por tendencia.
+
+elsoc %>% 
+  mutate(annio = factor(annio),
+         tendencia = factor(tendencia)) %>% 
+  group_by(annio, tendencia) %>% 
+  summarise(promedio_confianza = mean(confianza_gobierno, na.rm = TRUE)) %>% 
+  ggplot(aes(x = annio, y = promedio_confianza, color = tendencia, fill = tendencia)) +
+  geom_point() +
+  geom_line(aes(group = tendencia)) +
+  facet_wrap(~tendencia)
 
 
 # Aplique los siguientes ajustes al gráfico anterior:
@@ -66,6 +88,22 @@ head(elsoc)
 # - Añada una etiqueta correcta de los ejes x e y.
 # - Añada una leyenda: “Fuente: Elaboración propia a partir de los datos de ELSOC 2016-2022”.
 
+elsoc %>% 
+  mutate(annio = factor(annio),
+         tendencia = factor(tendencia)) %>% 
+  group_by(annio, tendencia) %>% 
+  summarise(promedio_confianza = mean(confianza_gobierno, na.rm = TRUE)) %>% 
+  ggplot(aes(x = annio, y = promedio_confianza, color = tendencia, fill = tendencia)) +
+  geom_point() +
+  geom_line(aes(group = tendencia)) +
+  theme_bw() +
+  labs(
+    title = "Título gráfico",
+    x = "Año",
+    y = "Promedio de confianza",
+    fill = "Tendencia",
+    color = "Tendencia"
+  )
 
 # Guardar el gráfico
 ggsave(filename = "output/grafico_longitudinal.png",
@@ -76,7 +114,9 @@ ggsave(filename = "output/grafico_longitudinal.png",
 
 # Ejercicio 5 (clase 5)
 
+permiso <- rio::import("data/pccochamo2016.csv")
 
+head(permiso)
 
 # 1. Obtenga el porcentaje por columna, según corresponda al tipo de variable y nivel de
 # medición.
@@ -99,7 +139,7 @@ data_df <- data.frame(
 data_df %>% 
   group_by(ANIO) %>% 
   summarise(total=sum(Frecuencia, na.rm=T)) %>% 
-  ggplot(aes(ANIO, total, group=1))+
-  geom_point()+
-  theme_minimal()
+  ggplot(aes(ANIO, total, group=1))+ #en algunos casos podría solicitarle el último argumento
+  geom_point()+ #líneas
+  theme_minimal() #temática
 
